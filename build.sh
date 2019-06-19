91d4ab42a6b5939a3bf4013272c13b38d62cba1c
wdurno <wdurno@kpmg.com>
2019-06-18 18:09:10 -0700

    server drafted

---
+++ build.sh
@@ -0,0 +1,2 @@
+docker build docker/ -t gcr.io/gdax-dnn/jupyter-server:0.1 #--squash 
+docker push gcr.io/gdax-dnn/jupyter-server:0.1
---
+++ docker/entrypoint.sh
@@ -0,0 +1,2 @@
+. venv/bin/activate 
+jupyter notebook --ip=127.0.0.1 --port=8888 --allow-root 
